8dca0aa0cb1def2c03c10e0503560bdcafabfae5
msahil9833 <imsahil9833@gmail.com>
2025-05-27 12:47:36 +0530

    Initial commit with bootstrap setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container py-5">
+      <h1 className="text-center mb-4">WebForge Builder 🚀</h1>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  );
+}
+
+export default App;
